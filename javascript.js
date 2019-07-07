f83268936fdbd22fd4a28b7f921f2a0734fad05c
ValentunSolodun <valentun.solodun@gmail.com>
2019-07-07 20:13:56 +0300

    slider fix bags

--- javascript.js
+++ javascript.js
@@ -4,165 +4,185 @@
 
 
 function SetSlider() {
-
 	var self = this;
 
-	this.generateStructure = function(data) {
+this.generateStructure = function(data) {
+	self.elemSet = document.getElementById(data);
+ 
+	self.sliderBox = document.createElement("div");
+	self.sliderBox.classList.add("slider-box");
+	self.sliderContent = document.createElement("div");
+	self.sliderContent.style.right = 0;
+	self.sliderContent.classList.add("slider-content");
+	self.sliderBox.appendChild(self.sliderContent);
 
-		self.elemSet = document.getElementById(data);
-	 
-		self.sliderBox = document.createElement("div");
-		self.sliderBox.classList.add("slider-box");
-		self.sliderContent = document.createElement("div");
-		self.sliderContent.style.right = 0;
-		self.sliderContent.classList.add("slider-content");
-		self.sliderBox.appendChild(self.sliderContent);
+	self.elemSet.appendChild(self.sliderBox);
 
-		self.elemSet.appendChild(self.sliderBox);
+	self.left = document.createElement("div");
+	self.left.classList.add("go-left");
+	self.left.id = "go-left";
+	self.right = document.createElement("div");
+	self.right.classList.add("go-right");
+	self.right.id = "go-right";
 
-		self.left = document.createElement("div");
-		self.left.classList.add("go-left");
-		self.left.id = "go-left";
-		self.right = document.createElement("div");
-		self.right.classList.add("go-right");
-		self.right.id = "go-right";
-
-		self.elemSet.appendChild(self.left);
-		self.elemSet.appendChild(self.right);
-	}
-
-	this.setImages = function(mas) {
-		mas.forEach( function(item, i, mas) {
-			var img = new Image();
-			img.src = item;
-			self.sliderContent.appendChild(img);
-			self.imgCountFull = i + 1;
-		});
-	}
-
-	this.params = function(par) {
-
-		self.sliderBox.style.width = par.quantityImg * par.widthImg + "px";
-		self.sliderContent.style.transition = par.animSpeed / 1000 + "s";
-
-		function arrowMove() {
-			self.direction = 0;
-
-			if(!par.arrow) {
-				self.left.style.display = self.right.style.display = "none";
-			};
-
-			self.right.onclick = function() {
-				self.direction = Math.min(self.direction + par.widthImg * par.countImg, par.widthImg * ( self.imgCountFull - par.quantityImg ) );
-				self.sliderContent.style.right = self.direction + "px";
-			};
-
-			self.left.onclick = function() {
-				self.direction = Math.max(0, self.direction - par.widthImg * par.countImg);
-				self.sliderContent.style.right = self.direction + "px";
-			};
-		}
-		arrowMove();
-
-		function draggableMove() {
-			if(!par.draggable) return;
-
-			self.sliderContent.onmousedown = function(event) {
-
-				var event = event || window.event;
-
-				var thumbCoords = getCoords(self.sliderContent);
-				var shiftX = event.pageX - thumbCoords.left;
-				var sliderCoords = getCoords(event.target);
-
-				document.onmousemove = function(event) {
-					var event = event || window.event;
-
-					var newLeft = event.pageX - shiftX - sliderCoords.left;
-
-					console.log(-newLeft);
-
-					if(-newLeft < 0) return;
-
-					if(-newLeft > par.widthImg * (self.imgCountFull - par.countImg) ) return;
-					
-					self.sliderContent.style.right = -newLeft  + "px";
-				}
-
-			  self.sliderContent.onmouseup = function() {
-			    document.onmousemove = self.sliderContent.onmouseup = null;
-
-			  };
-
-			  self.sliderContent.ondragstart = function() {
- 			 	return false;
-			  };
-
-				
-			};
-
-		}
-		draggableMove();
-
-		function loopMove() {
-			if(!par.loop.enable) {
-				clearInterval(timeLoopId);
-				return;	
-			}
-
-			var timeLoopId = setInterval(() => {
-
-				console.log(self.direction);
-				console.log()
-
-				if(self.direction >= par.widthImg * ( self.imgCountFull - par.quantityImg )) {
-					self.direction = 0;
-				}else{
-					self.direction = Math.min(self.direction + par.widthImg * par.countImg, par.widthImg * ( self.imgCountFull - par.quantityImg ) );
-				}
-
-				self.sliderContent.style.right = self.direction + "px";
-
-			}, par.loop.delay);
-		}
-		loopMove();
-
-		function getCoords(elem) {
-		  var box = elem.getBoundingClientRect();
-
-		  return {
-		    top: box.top + pageYOffset,
-		    left: box.left + pageXOffset
-		  };
-
-		}
-
-	}
+	self.sliderBox.appendChild(self.left);
+	self.sliderBox.appendChild(self.right);
+	return this;
 }
 
-var slide = new SetSlider();
-slide.generateStructure("one");
+this.setImages = function(mas) {
+	mas.forEach( function(item, i, mas) {
+		var img = new Image();
+		img.src = item;
+		self.sliderContent.appendChild(img);
+		self.imgCountFull = i + 1;
+	});
+	return this;
+}
 
-slide.setImages([
+this.params = function(par) {
+
+	self.sliderBox.style.width = par.quantityImg * par.widthImg + "px";
+	self.sliderContent.style.transition = par.animSpeed / 1000 + "s";
+
+	function setArrowIcon() {
+		self.left.innerHTML = par.arrow.left;
+		self.right.innerHTML = par.arrow.right;
+
+	}
+	setArrowIcon();
+
+	function arrowMove() {
+		if(!par.arrow.enable) {
+			self.left.style.display = self.right.style.display = "none";
+			return;
+		};
+
+		self.direction = 0;
+
+		self.right.onclick = function() {
+			self.direction = Math.min(self.direction + par.widthImg * par.countImg, par.widthImg * ( self.imgCountFull - par.quantityImg ) );
+			self.sliderContent.style.right = self.direction + "px";
+		};
+
+		self.left.onclick = function() {
+			self.direction = Math.max(0, self.direction - par.widthImg * par.countImg);
+			self.sliderContent.style.right = self.direction + "px";
+		};
+	}
+	arrowMove();
+
+	function draggableMove() {
+		if(!par.draggable) return;
+
+		self.sliderContent.onmousedown = function(event) {
+
+			var event = event || window.event;
+
+			var thumbCoords = getCoords(self.sliderContent);
+			var shiftX = event.pageX - thumbCoords.left;
+			var sliderCoords = getCoords(event.target);
+
+			document.onmousemove = function(event) {
+				var event = event || window.event;
+
+				var newLeft = event.pageX - shiftX - sliderCoords.left;
+
+				if(-newLeft < 0) return;
+
+				if(-newLeft > par.widthImg * (self.imgCountFull - par.quantityImg) ) return;
+				
+				self.sliderContent.style.right = -newLeft  + "px";
+			}
+
+		  self.sliderContent.onmouseup = function() {
+		  	document.onmousemove = self.sliderContent.onmouseup = null;
+		  };
+
+		  self.sliderContent.ondragstart = function() {
+			return false;
+		  };
+		};
+
+	}
+	draggableMove();
+
+	function loopMove() {
+		if(!par.loop.enable) {
+			clearInterval(timeLoopId);
+			return;	
+		}
+
+		var timeLoopId = setInterval(() => {
+
+			if(self.direction >= par.widthImg * ( self.imgCountFull - par.quantityImg )) {
+				self.direction = 0;
+			}else{
+				self.direction = Math.min(self.direction + par.widthImg * par.countImg, par.widthImg * ( self.imgCountFull - par.quantityImg ) );
+			}
+
+			self.sliderContent.style.right = self.direction + "px";
+
+		}, par.loop.delay);
+	}
+	loopMove();
+
+	function getCoords(elem) {
+	  var box = elem.getBoundingClientRect();
+
+	  return {
+	    top: box.top + pageYOffset,
+	    left: box.left + pageXOffset
+	  };
+
+	}
+	return this;
+}
+}
+
+var slide = new SetSlider().generateStructure("one").setImages([
 	"img/1.jpg",
 	"img/2.jpg",
 	"img/3.jpg",
 	"img/4.jpg",
 	"img/5.jpg"
-]);
+]).params({
+	arrow : {
+		enable : true,
+		left : "<img src='left.svg'>",
+		right : "<img src='right.svg'>"
+	},
+	loop : {
+		enable : true,
+		delay : 5000
+	},
+	draggable : false,
+	animSpeed : 700,
+	countImg : 2,
+	widthImg : 277,
+	quantityImg : 2
+});
 
-slide.params({
-	arrow : true,
+var slide2 = new SetSlider().generateStructure("two").setImages([
+	"img/1.jpg",
+	"img/2.jpg",
+	"img/3.jpg",
+	"img/4.jpg"
+]).params({
+	arrow : {
+		enable : true,
+		left : "<img src='left.svg'>",
+		right : "<img src='right.svg'>"
+	},
 	loop : {
 		enable : true,
 		delay : 1000
 	},
 	draggable : false,
-	animSpeed : 100,
-	countImg : 1,
+	animSpeed : 200,
+	countImg : 2,
 	widthImg : 277,
 	quantityImg : 2
 });
-
 
 })();
